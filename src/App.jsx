2be1e2b7983aee1cdd6a340e65d16527a5b34fbb
Hannan <l1f21bsse0375@ucp.edu.pk>
2025-03-26 10:24:51 +0500

    Signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import IdeaSelection from "./pages/IdeaSelection";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/idea-selection" element={<IdeaSelection />} />
       </Routes>
     </Router>
